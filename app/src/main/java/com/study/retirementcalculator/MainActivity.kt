dbb2c68299303de4e383610e969caed5aba9bbf5
tiksontom <tiksontom@gmail.com>
2022-03-22 17:00:37 +0530

    add app center

--- app/src/main/java/com/study/retirementcalculator/MainActivity.kt
+++ app/src/main/java/com/study/retirementcalculator/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        AppCenter.start(application, "e10a3977-599a-43bd-9132-cf9e615ead6b", Analytics::class.java, Crashes::class.java)
+
     }
 }
