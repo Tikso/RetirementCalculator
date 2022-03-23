8a5736bc5c85a5b20c2256567e0e6ca03ddcecf6
tiksontom <tiksontom@gmail.com>
2022-03-23 11:51:17 +0530

    commit

--- app/src/main/java/com/study/retirementcalculator/MainActivity.kt
+++ app/src/main/java/com/study/retirementcalculator/MainActivity.kt
@@ -2,6 +2,10 @@ package com.study.retirementcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +13,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         AppCenter.start(application, "e10a3977-599a-43bd-9132-cf9e615ead6b", Analytics::class.java, Crashes::class.java)
 
+        val clickMe = findViewById<Button>(R.id.clickMe)
+        clickMe.setOnClickListener {
+            Crashes.generateTestCrash()
+        }
     }
 }
